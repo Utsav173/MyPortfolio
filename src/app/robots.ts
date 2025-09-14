2d974db7e3d8a2d0a43c1978cb49069611f65c72
Utsav173 <81065457+Utsav173@users.noreply.github.com>
2025-09-14 16:16:48 +0530

    fix robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,13 +3,11 @@ import { SITE_URL } from '@/lib/config';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: ['/', '/_next/static/'],
-        disallow: '/api/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
